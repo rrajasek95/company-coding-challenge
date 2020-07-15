99800d2da0d9a88afdf0d82684202e6fb762c1a1
Rishi Rajasekaran <rajasekaran.rishi@gmail.com>
2020-07-15 15:20:47 -0700

    Add sqlite queries

--- db.py
+++ db.py
@@ -1,3 +1,4 @@
+import json
 import sqlite3
 
 class DB(object):
@@ -41,15 +42,22 @@ class DB(object):
         :param id:
         :return:
         """
-        return DB.quizzes.get(quiz_id)
+        c = self.db.cursor()
+        c.execute('''SELECT * FROM questions WHERE quiz_id=?''', quiz_id)
+        questions = [{"text": row[0]} for row in c.fetchall()]
+        c.close()
+        return questions
 
     def submit_quiz_responses(self, quiz_id, user_id, responses):
-        quiz = DB.responses.get(quiz_id)
-        quiz[user_id] = responses
-
+        c = self.db.cursor()
+        c.execute('''INSERT INTO responses VALUES (?, ?, ?)''', quiz_id, user_id, json.dumps(responses))
+        c.close()
         return True
 
     def get_all_quizzes(self):
-        return list(DB.quizzes.keys())
+        c = self.db.cursor()
+        c.execute('''SELECT * FROM quizzes''')
+        quizzes = [row[0] for row in c.fetchall()]
+        return quizzes
 
 db = DB('quiz.db')
